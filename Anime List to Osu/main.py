52c14edfd70540fa7e6e94064191f2bbe335d6eb
Raj Pandya <rajpandya737@gmail.com>
2023-07-29 14:44:37 -0400

    first commit

--- Anime List to Osu/main.py
+++ Anime List to Osu/main.py
@@ -2,9 +2,8 @@ from ossapi import Ossapi
 import config
 
 # create a new client at https://osu.ppy.sh/home/account/edit#oauth
-api = Ossapi(config.id, config.password)
+api = Ossapi(config.client_id, config.client_secret)
 
 # see docs for full list of endpoints
-print(api.user("tybug").username)
-print(api.user(12092800, mode="osu").username)
-print(api.beatmap(221777).id)
+
+print((api.search_beatmapsets("tokyo ghoul unravel"))[0])
