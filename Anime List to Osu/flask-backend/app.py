9dcf24c1cf556d1450b51cb7f14ed21dd239a01c
Raj Pandya <rajpandya737@gmail.com>
2023-08-01 00:10:45 -0400

    temp commit

--- Anime List to Osu/flask-backend/app.py
+++ Anime List to Osu/flask-backend/app.py
@@ -1,28 +1,18 @@
-from flask import Flask, render_template, request, jsonify
-from data_processing import convertor
+from flask import Flask, request, jsonify, render_template
+#from data_processing import convertor
 import os
-from dotenv import load_dotenv
+#from dotenv import load_dotenv
 
-load_dotenv
+#load_dotenv()
 app = Flask(__name__)
-app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
+#app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 
 # if any errors arise, try changing yarn.lock file back to original
 
 @app.route("/")
-def things():
-    return render_template("index.html", token="Flask+React")
-
-
-@app.route("/process", methods=["POST"])
-def process_form():
-    if request.method == "POST":
-        username = request.form["username"]
-        anime_list = jsonify(convertor(username))
-        print(anime_list)
-        return anime_list
-
+def home():
+    return render_template("index.html", token="Hello Flask+React") # token is used to test if the connection is working
 
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    app.run(debug=True)
