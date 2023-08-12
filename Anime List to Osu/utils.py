594364c5e44354daa9f5453d259e55fc346a187c
Raj Pandya <99134716+RajPandya737@users.noreply.github.com>
2023-08-12 00:01:14 -0400

    Delete Anime List to Osu directory

--- Anime List to Osu/utils.py
+++
@@ -1,9 +0,0 @@
-get_first_non_empty = lambda data: next((item for item in data if item), [None])
-
-remove_blank_entries = lambda lst: [entry.strip() for entry in lst if entry.strip()]
-
-decode_unicode = lambda lst: [
-    entry.encode("ascii", "ignore").decode("utf-8") for entry in lst
-]
-
-convert_to_string = lambda input_string: input_string.encode().decode("unicode_escape")
